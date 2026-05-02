12ae19ae8f09d453bd2348e92cb0c394124affad
Alejandro Trejo <istrejo2106@gmail.com>
2026-05-02 13:43:31 +0100

    fix(core): use SafeAreaView from react-native-safe-area-context in Screen

--- src/modules/core/components/Screen.tsx
+++ src/modules/core/components/Screen.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  type ViewStyle,
-} from 'react-native';
+import { ScrollView, View, type ViewStyle } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTheme } from '@core/theming';
 
 interface ScreenProps {
